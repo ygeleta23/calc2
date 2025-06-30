41ca2d9b3093c794ecb48380bfb795e437acb27a
Yonathan Geleta <yonathan.geleta@seo-usa.org>
2025-06-30 17:25:24 +0000

    fixed style issues

--- calc.py
+++ calc.py
@@ -8,20 +8,19 @@ class Calc:
     def sub(self, a, b):
         """Subtract two numbers"""
         return a - b
-    
+
     def mul(self, a, b):
         """Multiply two numbers"""
         return a * b
-    
+
     def div(self, a, b):
         """Divide two numbers"""
         return a / b
-    
+
     def pow(self, a, b):
         """Raise a to the power of b"""
         return a ** b
-    
+
     def sqrt(self, a):
         """Return the square root of a"""
         return a ** 0.5
-    
